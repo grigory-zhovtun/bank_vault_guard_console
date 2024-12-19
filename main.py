8bca760e2051c1cbe13383d2d2fd07bbd3024399
grigory-zhovtun <grigory.zhovtun@gmail.com>
2024-12-19 16:01:59 +0500

    fix output with results

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcard_all = Passcard.objects.all()
+
+    object_fields = {
+        'is_active': passcard_all[0].is_active,
+        'created_at': passcard_all[0].created_at,
+        'passcode': passcard_all[0].passcode,
+        'owner_name': passcard_all[0].owner_name,
+    }
+
+    active_passcard = Passcard.objects.filter(is_active=True)
+
+    print(f'Всего пропусков {Passcard.objects.count()}')
+    print(f'Активных пропусков {len(active_passcard)}') # noqa: T001
